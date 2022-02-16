0236cec7b1524428392140dcf7f81d86cd60c620
tonyv <tonyvilla77@gmail.com>
2022-02-15 20:27:45 -0800

    cleaaned up routes

--- client/src/routes/config/config.js
+++ client/src/routes/config/config.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import BrowseGenre from '../BrowseGenre/BrowseGenre';
-import BrowseMovies from '../BrowseMovies/BrowseMovies';
-import Analyze from '../Compare/Analyze';
-import CompareSetup from '../Compare/CompareSetup';
 import Home from '../Home/Home';
-import Profile from '../Profile/Profile';
-import ShowMovie from '../ShowMovie/ShowMovie';
 
 export default (
   <Routes>
