27a0df3bdd73c4a57fd4f8dc8fb49195eb1b389b
tonyv <tonyvilla77@gmail.com>
2022-02-16 12:41:26 -0800

    changed test show income ctrl

--- server/routes/incomeRoutes.js
+++ server/routes/incomeRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const ctrl = require('../controllers');
 
 //routes
-router.get('/showIncome/:user_id', ctrl.income.showIncome);
+router.get('/showIncome', ctrl.income.showIncome);
 
 // exports
 module.exports = router;
