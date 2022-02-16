be2a1d480ff0243d8aecab046f3ed4a914601f81
tonyv <tonyvilla77@gmail.com>
2022-02-16 13:22:46 -0800

    testing expenses routes

---
+++ server/controllers/expenseController.js
@@ -0,0 +1,44 @@
+const pool = require('../db_config/db');
+require('dotenv').config();
+
+const showExpenses = async (req, res) => {
+  try {
+    const expenses = await pool.query('SELECT * FROM expenses WHERE user_id = $1', [req.params.user_id]);
+
+    res.status(200).json({ expenses: expenses.rows });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Server Error');
+  }
+};
+
+// const addIncome = async (req, res) => {
+//   try {
+//     const { user_id, fixed_income, variable_income } = req.body;
+
+//     const income = await pool.query(
+//       'INSERT INTO income (user_id, fixed_income, variable_income) VALUES ($1,$2,$3) RETURNING *',
+//       [user_id, fixed_income, variable_income]
+//     );
+
+//     res.status(200).json({ income: income.rows[0] });
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// };
+
+// const deleteIncome = async (req, res) => {
+//   try {
+//     const { id } = req.params;
+
+//     const deleteIncome = await pool.query('DELETE FROM income WHERE id = $1', [id]);
+
+//     res.json('Income item was deleted');
+//   } catch (err) {
+//     res.status(500).send('Server Error');
+//   }
+// };
+
+module.exports = {
+  showExpenses,
+};
