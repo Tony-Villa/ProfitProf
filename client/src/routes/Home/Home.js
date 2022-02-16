2ae8a99ebd62d9c7e9a1d024e393aab3206cecd7
Leonard Moses <qalexeon@Xavier.lan>
2022-02-16 16:11:05 -0500

    Roughly blocked Splash Page.

--- client/src/routes/Home/Home.js
+++ client/src/routes/Home/Home.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import AuthForm from '../../components/auth/AuthForm/AuthForm';
 import { UserContext } from '../../Context/UserContext';
+import { Link } from 'react-router-dom';
 
 const Home = ({ isUser, setAuth }) => {
   const { user } = useContext(UserContext);
@@ -9,11 +10,39 @@ const Home = ({ isUser, setAuth }) => {
   console.log(isUser);
   return (
     <div className='home'>
-      <h1>Hello, Profit Prof</h1>
+      <div className='splash_title_div'>
+        <div className='splash_title'>
+          <h1>ProfitProf</h1>
+        </div>
+        
 
-      <div className='authform'>
+        <div className='splash_nav'>
+          <Link to="howitworks" className='splah_nav_link'>
+            <h2>How It Works</h2>
+          </Link>
+          <Link to="pricing" className='splah_nav_link'>
+            <h2>Pricing</h2>
+          </Link>
+          <Link to="resources" className='splah_nav_link'>
+            <h2>Resources</h2>
+          </Link>
+        </div>
+      </div>
+
+    <div className='splash_main_div'>
+
+      <div className='splash_main_left'>
+        <h1>Managing financial goals has never been easier</h1>
+      </div>
+      
+
+      <div className='splash_main_right'>
         <AuthForm setAuth={setAuth} />
       </div>
+    </div>
+
+
+
       
     </div>
   );
