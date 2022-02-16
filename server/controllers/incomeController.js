416e1a45d182c09e117fb81752cd6756f5bd2c60
tonyv <tonyvilla77@gmail.com>
2022-02-16 13:03:25 -0800

    added delete and post routes

--- server/controllers/incomeController.js
+++ server/controllers/incomeController.js
@@ -1,17 +1,6 @@
 const pool = require('../db_config/db');
 require('dotenv').config();
 
-// const showIncome = async (res, req) => {
-//   try {
-//     const income = await pool.query('SELECT * FROM income WHERE user_id = $1', [req.params.user_id]);
-
-//     res.status(200).json({ income: income.rows });
-//   } catch (err) {
-//     console.log(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// };
-
 const showIncome = async (req, res) => {
   try {
     const income = await pool.query('SELECT * FROM income WHERE user_id = $1', [req.params.user_id]);
