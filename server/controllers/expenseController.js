6af603c9ddcf776901b61195026ae2676c49c3b9
tonyv <tonyvilla77@gmail.com>
2022-02-16 13:33:56 -0800

    fixed type expense controller

--- server/controllers/expenseController.js
+++ server/controllers/expenseController.js
@@ -16,12 +16,12 @@ const addExpense = async (req, res, next) => {
   try {
     const { user_id, expense_type, expense_subtype, description, value } = req.body;
 
-    const newReview = await pool.query(
+    const newExpense = await pool.query(
       'INSERT INTO expenses (user_id, expense_type,expense_subtype,description, value) VALUES ($1,$2,$3,$4,$5) RETURNING *',
       [user_id, expense_type, expense_subtype, description, value]
     );
 
-    res.json({ review: newReview.rows[0] });
+    res.json({ expense: newExpense.rows[0] });
   } catch (err) {
     console.log(err.message);
     res.status(500).send('Server Error');
