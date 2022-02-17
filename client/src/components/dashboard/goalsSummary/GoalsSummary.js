f26039b37ed268cdd92fc3c5f0f01d11ad8ccead
tonyv <tonyvilla77@gmail.com>
2022-02-16 21:39:58 -0800

    goals component

---
+++ client/src/components/dashboard/goalsSummary/GoalsSummary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GoalsSummary = () => {
+  return (
+    <div>
+      <p>hello</p>
+      <p>hi</p>
+    </div>
+  );
+};
+
+export default GoalsSummary;
---
+++ client/src/components/shared/GoalCard/GoalCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GoalCard = () => {
+  return (
+    <div>
+      <p>goal card</p>
+      <p>goals woo</p>
+    </div>
+  );
+};
+
+export default GoalCard;
