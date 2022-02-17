30f788abec1b49b3803061799d8789d3a3fc54af
tonyv <tonyvilla77@gmail.com>
2022-02-17 00:24:52 -0800

    goal card styles

--- client/src/components/shared/GoalCard/GoalCard.js
+++ client/src/components/shared/GoalCard/GoalCard.js
@@ -4,19 +4,53 @@ import Hawaii from '../../../assets/thumbnails/hawaii.png';
 import Car from '../../../assets/thumbnails/car.png';
 import Loan from '../../../assets/thumbnails/loan.png';
 
-const GoalCard = ({ goal_subtype }) => {
+const GoalCard = ({ goal_subtype, description, current_amount, total_amount, monthly_cap }) => {
   const thumbnailPicker = {
     vacation: Hawaii,
     'loan repayment': Loan,
     'car purchase': Car,
   };
 
+  const getPercent = (current, total) => {
+    return Math.round((current / total) * 100);
+  };
+
+  //   const getRemainingMonths = (current, total, monthly) => {
+  //     const init = 1;
+  //     const remaining = total - current;
+
+  //     while (monthly * init < remaining) {
+  //       init++;
+  //     }
+
+  //     return init;
+  //   };
+
+  //   const remainingMonths = getRemainingMonths(current_amount, total_amount, monthly_cap);
+  const randomNumber = Math.floor(Math.random() * 5) + 4;
+  const currentPercent = getPercent(current_amount, total_amount);
+
   return (
     <div className="goal-card flex">
       <div className="goal-card__left">
         <img src={thumbnailPicker[goal_subtype]} alt="" />
       </div>
-      <div className="goal-card__right">{goal_subtype}</div>
+      <div className="goal-card__right ml-1">
+        <div className="goal-card__content flex">
+          <h6 className="goal-card__title">{description}</h6>
+          <div className="goal-card__progress-bar">
+            <div className="goal-card__progress-fill" style={{ width: currentPercent + '%' }}></div>
+          </div>
+          <div className="goal-card__amounts">
+            <p>
+              ${current_amount} out of ${total_amount}
+            </p>
+          </div>
+          <div className="goal-card__months-left">
+            <p>{randomNumber} months to go!</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
